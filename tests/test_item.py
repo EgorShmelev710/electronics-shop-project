2015f1190b417d4c4326d55b5b0fd28ece4090a3
Egor Shmelev <shmelevyegor25@gmail.com>
2024-01-07 19:38:07 -0300

    added test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def coll():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_init(coll):
+    assert coll.name == 'Смартфон'
+
+
+def test_calculate_total_price(coll):
+    coll.calculate_total_price()
+    assert coll.total_price == 200000
+
+
+def test_apply_discount(coll):
+    Item.pay_rate = 0.5
+    coll.apply_discount()
+    assert coll.price == 5000
