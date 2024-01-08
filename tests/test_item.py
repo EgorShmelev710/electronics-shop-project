7ba9240e1c6dddf393656c6208f4c53c157a7150
Egor Shmelev <shmelevyegor25@gmail.com>
2024-01-08 17:49:45 -0300

    changes to test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,10 +8,6 @@ def coll():
     return Item("Смартфон", 10000, 20)
 
 
-def test_init(coll):
-    assert coll.name == 'Смартфон'
-
-
 def test_calculate_total_price(coll):
     coll.calculate_total_price()
     assert coll.total_price == 200000
@@ -21,3 +17,20 @@ def test_apply_discount(coll):
     Item.pay_rate = 0.5
     coll.apply_discount()
     assert coll.price == 5000
+
+
+def test_get_name(coll):
+    assert coll.get_name == 'Смартфон'
+
+
+def test_get_name_setter(coll):
+    coll.get_name = 'Смартфон105647747'
+    assert coll.name == 'Смартфон10'
+    coll.get_name = 'Смарт'
+    assert coll.name == 'Смарт'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
